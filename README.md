0b81231d75c86cb5fb0d936b37e9b6cc21ecce4d
sauercrowd <sauercrowd@posteo.no>
2019-08-22 13:58:36 +0200

    added usage, support of start & stop

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # rqubit
+
+Simplify the usage of rigettis compiler and qvm
+
+
+## Usage
+
+It spins up a quilc and qvm instance so you don't have to open additional sessions
+
+```python
+from rqubit import Quomputer
+
+from pyquil import Program
+from pyquil import get_qc
+from pyquil.gates import *
+
+with Quomputer():
+    
+    p = Program()
+    ro = p.declare('ro', 'BIT', 1)
+    p += X(0)
+    p += MEASURE(0, ro[0])
+    
+    qc = get_qc('1q-qvm')
+    executable = qc.compile(p)
+    result = qc.run(executable)
+    
+    print(result)
+```
+
+or
+
+```python
+from rqubit import Quomputer
+
+q = Quomputer()
+q.start()
+
+p = Program()
+ro = p.declare('ro', 'BIT', 1)
+p += X(0)
+p += MEASURE(0, ro[0])
+
+qc = get_qc('1q-qvm')
+executable = qc.compile(p)
+result = qc.run(executable)
+
+print(result)
+
+q.stop()
+```
+
--- rqubit/quomputer.py
+++ rqubit/quomputer.py
@@ -1,18 +1,72 @@
+import atexit
 import subprocess
+import socket
+import time
+import logging
+import os
+
+logger = logging.getLogger(__name__)
 
 class Quomputer:
 
     def __init__(self):
-        pass
+        self.qvm_proc = None
+        self.quilc_proc = None
+        atexit.register(self.stop)
 
-    def start(self):
-        self._start_qvm()
-        self._start_quilc()
+    def __enter__(self):
+        self.start()
+        return self
 
-    def _start_qvm(self):
-        self.qvm_proc = subprocess.Popen(['/usr/local/bin/qvm', '-S'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
 
-    def _start_quilc(self):
-        self.quilc_proc = subprocess.Popen(['/usr/local/bin/quilc', '-S'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def start(self, basepath='/usr/local/bin'):
+        """start qvm and quilc"""
+        self._start_qvm(basepath)
+        self._start_quilc(basepath)
 
+    def stop(self):
+        """stop qvm and quilc if running"""
+        logger.warning("Shutting down")
+        if self.qvm_proc:
+            self.qvm_proc.kill()
+            self.qvm_proc = None
+
+        if self.quilc_proc:
+            self.quilc_proc.kill()
+            self.quilc_proc = None
+
+    def _start_qvm(self, basepath):
+        self.qvm_proc = subprocess.Popen([os.path.join(basepath, 'qvm'), '-S'], 
+                stdout=subprocess.PIPE)
+        self._poll('127.0.0.1', 5000)
+
+    def _start_quilc(self, basepath):
+        self.quilc_proc = subprocess.Popen([os.path.join(basepath, 'quilc'), '-S'], 
+                stdout=subprocess.PIPE)
+        self._poll('127.0.0.1', 5555)
+
+    def _poll(self, address, port, timeout_seconds=60, sleep_seconds=1):
+        """try to connect to the specified address and 
+            port to make sure things are properly up and running
+        """
+        used_time = 0
+        while used_time < timeout_seconds:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                logger.warn('Trying to connect to {}:{}'.format(address, port))
+                s.connect((address, port))
+                # connected, we can stop polling
+                return 
+            except Exception as e:
+                logger.info("Cannot connect to {}:{} just yet".format(address, port))
+            finally:
+                s.close()
+
+            used_time += sleep_seconds
+            time.sleep(sleep_seconds)
+
+        # reached timeout, raise exception
+        raise TimeoutError('Reached timeout of {} seconds'.format(timeout_seconds))
 
